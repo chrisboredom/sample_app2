c0e69120912fa970ab5babe4f82bd040b58d3979
Chris Mangano <chris.mangano@me.com>
2014-01-19 12:41:24 -0500

    Finish layout and routes

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,7 @@
+include ApplicationHelper
+
+# I should be able to comment out the code below but tests fail if I do
+
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
